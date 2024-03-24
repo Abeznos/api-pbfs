0479b6a0958023552ae56c554b1b1aa8edd4113e
Anton Beznos <abeznos@yandex.ru>
2024-03-25 00:23:24 +0300

    Создал проект

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
