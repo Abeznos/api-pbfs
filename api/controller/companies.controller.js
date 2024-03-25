9ff89ca598aa9cff6a56487a722d392ed314e7c1
Abeznos <Abeznos@yandex.ru>
2024-03-25 11:25:26 +0300

    ad

--- api/controller/companies.controller.js
+++ api/controller/companies.controller.js
@@ -1,9 +1,11 @@
 const db = require('../db');
+console.log(db);
 
 class CompaniesController {
     async createCompany(req, res) {
         const {name, logo, pb_id} = req.body;
-        const newCompany = await db.query('INSERT INTO companies (name, logo, pb_id) values ($1, $2, $3) RETURNING *', [name, logo, pb_id])
+        console.log(db);
+        //const newCompany = await db.query('INSERT INTO companies (name, logo, pb_id) values ($1, $2, $3) RETURNING *', [name, logo, pb_id]);
         res.json(newCompany);
     }
     async getCompany(req, res) {
