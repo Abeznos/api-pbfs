b189fc7f196b57e752ea8f1a050c8ad67387d780
Abeznos <Abeznos@yandex.ru>
2024-03-25 14:08:44 +0300

    ыв

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: "default",
   password: "3dxnbF7XkQgu",
-  host: "ep-delicate-limit-a2izpwb3-pooler.eu-central-1.aws.neon.tech",
+  host: "postgres://default:3dxnbF7XkQgu@ep-delicate-limit-a2izpwb3-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
   port: "5432",
   database: "verceldb"
 });
