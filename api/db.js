12ec38ef837c1b9adc26aae22a912bb3e7c9b322
Anton Beznos <abeznos@yandex.ru>
2024-03-25 09:33:10 +0300

    dfsdf

--- api/db.js
+++ api/db.js
@@ -1,6 +1,7 @@
-const { Pool } = require('pg').Pool;
-require('dotenv').config();
+import pg from 'pg';
+
+const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: "postgres://default:3dxnbF7XkQgu@ep-delicate-limit-a2izpwb3-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  connectionString: process.env.POSTGRES_URL ,
 })
