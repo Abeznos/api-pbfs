8775d50013a2abfea899841188f59763f26f2c26
Anton Beznos <abeznos@yandex.ru>
2024-03-26 02:19:23 +0300

    Настройка

--- api/index.js
+++ api/index.js
@@ -1,58 +1,26 @@
 require('dotenv').config();
 
 const express = require("express");
-//const companiesRouter = require('./routes/companies.routes');
-
 const app = express();
-const { sql } = require('@vercel/postgres');
+const db = require('./queries');
 
-const bodyParser = require('body-parser');
+//console.log(db);
+
+const bodyParser = require('body-parser')
 const path = require('path');
 
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'));
-
-app.use(express.json());
-//app.use('/', companiesRouter);
-
-app.post('/companies', urlencodedParser, async (req, res) => {
-	try {
-		const newCompany = await sql`INSERT INTO companies (name, logo, pb_id) VALUES (${req.body.name}, ${req.body.logo}, ${req.body.pb_id});`;
-        res.json(newCompany.rows[0]);
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
-});
-
-app.get('/page', urlencodedParser, async (req, res) => {
-	try {
-        const id = req.params.page_id;
-        console.log(id);
-        //const page = await sql`SELECT * FROM pages were page_id = ${id}`;
-        //if (page && page.rows.length > 0) {
-        //   res.json(page.rows[0]);
-        //} else {
-        //    res.status(404).send('Page not found');
-        //}
-    } catch (error) {
-		console.error(error);
-		res.status(500).send('Error retrieving users');
-    }
-});
-
-
-//async createCompany(req, res) {
-//    const {name, logo, pb_id} = req.body;
-//    const newCompany = await db.query('INSERT INTO companies (name, logo, pb_id) VALUES ($1, $2, $3) RETURNING *', [name, logo, pb_id]);
-//    res.json(newCompany);
-//}
-
+app
+  .use(express.static('public'))
+  .use(bodyParser.json())
+  .use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(process.env.PORT, () => console.log(`Server ready on port ${process.env.PORT}`));
 
 module.exports = app;
