be8273e8e857750870644fcf392098bce08dc970
Abeznos <Abeznos@yandex.ru>
2024-03-25 10:55:35 +0300

    dfsf

--- api/db.js
+++ api/db.js
@@ -1,7 +1,18 @@
-import pg from 'pg';
-
-const { Pool } = pg;
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
-})
+  user: "default",
+  password: "3dxnbF7XkQgu",
+  host: "ep-delicate-limit-a2izpwb3-pooler.eu-central-1.aws.neon.tech",
+  port: "5432",
+  database: "verceldb"
+});
+
+
+module.exports = pool;
+
+//const { Pool } = pg;
+//
+//const pool = new Pool({
+//  connectionString: process.env.POSTGRES_URL ,
+//})
