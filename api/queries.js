caeebccd3c96ded465b3e408d781182a1ee5ed9c
Abeznos <Abeznos@yandex.ru>
2024-03-29 19:45:43 +0300

    sdsd

--- api/queries.js
+++ api/queries.js
@@ -4,25 +4,31 @@ const { v4: uuidv4 } = require('uuid');
 const Pool = require('pg').Pool
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL
-  //user: process.env.POSTGRES_USER,
-  //host: process.env.POSTGRES_HOST,
-  //database: process.env.POSTGRES_DATABASE,
-  //password: process.env.POSTGRES_PASSWORD,
-  //port: process.env.POSTGRES_PORT,
 });
 
 
 const createCompany = async (request, response) => {
-  const { name, logo, pb_id } = request.body;
-  //const id = uuidv4();
+  const { company_name, tariff, company_status, logo, pb_id } = request.body;
+  const id = uuidv4();
   try {
-    await pool.query('INSERT INTO companies (name, logo, pb_id) VALUES ($1, $2, $3)', [name, logo, pb_id ], (error, results) => {
-      response.status(201).send(`Company added with ID: ${results.insertId}`);
+    await pool.query('INSERT INTO companies (company_id, company_name, tariff) VALUES ($1, $2, $3)', [id, company_name, tariff], (error, results) => {
+      response.status(201).send(`Company added with ID: ${results.rows[0].id}`);
     });
   } catch (error) {
-
+      res.status(500).send('Error adding compony');
   }
 }
+
+//const createCompany = (request, response) => {
+//  const { company_name, logo } = request.body
+//
+//  pool.query('INSERT INTO companies (company_name, logo) VALUES ($1, $2) RETURNING *', [company_name, logo], (error, results) => {
+//    if (error) {
+//      throw error
+//    }
+//    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+//  })
+//}
 
 const getCompanies = async (request, response) => {
   try {
@@ -49,10 +55,10 @@ const getCompanyByID = async (request, response) => {
 }
 
 const deleteCompany = async (request, response) => {
-  const id = parseInt(request.params.id)
+  const id = request.params.id;
 
   try {
-    await pool.query('DELETE FROM companies WHERE id = $1', [id], (error, results) => {
+    await pool.query('DELETE FROM companies WHERE company_id = $1', [id], (error, results) => {
       response.status(200).send(`Company deleted with ID: ${id}`)
     });
  } catch (error) {
