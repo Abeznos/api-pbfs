d60d6ced32ef1bf40ba45ed7f3d8386162802109
Abeznos <Abeznos@yandex.ru>
2024-10-01 19:54:06 +0300

    AssessmentService

--- api/services/AssessmentService.js
+++ api/services/AssessmentService.js
@@ -3,8 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 class AssessmentService {
     async saveAssessment(data) {
-        //console.log(db)
-        //console.log(process.env.TEST)
+        //console.log(process.env.POSTGRES_URL)
+        console.log(data)
 
         const id = uuidv4();
 
@@ -12,8 +12,8 @@ class AssessmentService {
         const { name, phone, email, legal } = data.userData
         const answers = data.questions
 
-        const assessment = await db.query('INSERT INTO assessments (assessment_id, company_name, company_id, point, chanal, grade) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-        [id, company, company_id, point, chanal, grade])
+        const assessment = await db.query('INSERT INTO assessments (assessment_id, company_name, company_id, point, chanal, grade, legal_approved) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [id, company, company_id, point, chanal, grade, legal])
         return assessment.rows[0]
     }
 
